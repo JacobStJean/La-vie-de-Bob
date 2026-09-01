b6f4fbd490ee35ec87488386b1209387ca92293f
Jacob <2482265@cegepoutaouais.qc.ca>
2026-08-31 21:18:17 -0400

    Premiere classe avec diagramme

--- LaVieDeBob/Program.cs
+++ LaVieDeBob/Program.cs
@@ -1,1 +1,66 @@
-﻿Console.WriteLine("Hello, World!");
+﻿classDiagram
+
+class Humain
+{
+    +string Nom
+    +Compte Compte
+    +deposerArgent(double montant)
+    +retirerArgent(double montant)
+}
+
+class Programmeur
+{
+    +double SalaireHoraire
+    +travailler(double heures)
+    +nourrirAnimal(Animal animal)
+}
+
+class Compte
+{
+    +double Solde
+    +deposer(double montant)
+    +retirer(double montant)
+}
+
+class Animal
+{
+    <<abstract>>
+    +string Nom
+    +bool A_Faim
+    +DateTime DernierRepas
+    +double CoutRepas
+    +int DelaiRepas
+    +exprimer()
+    +manger(DateTime date)
+    +peutManger(DateTime date)
+}
+
+class Chat
+{
+    +exprimer()
+}
+
+class Chien
+{
+    +exprimer()
+}
+
+class Horloge
+{
+    +DateTime DateActuelle
+    +avancerTemps(double heures)
+}
+
+Humain <| --Programmeur
+Animal <| --Chat
+Animal <| --Chien
+
+Humain "1" *-- "1" Compte : possède
+
+Programmeur "1"-- > "1" Chat : possède
+Programmeur "1"-- > "1" Chien : possède
+
+Programmeur --> Horloge : utilise
+Animal --> Horloge : utilise
+
+
---
+++ LaVieDeBob/humain.cs
@@ -0,0 +1,15 @@
+﻿public class humain
+{
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
+    public int Age { get; set; }
+
+
+
+    public humain(string nom, string prenom, int age)
+    {
+        Nom = nom;
+        Prenom = prenom;
+        Age = age;
+    }
+}
